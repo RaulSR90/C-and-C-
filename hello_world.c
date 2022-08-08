f50934b3909d8ca122e5dabca4c91c3853e32276
RaulSR90 <75151613+RaulSR90@users.noreply.github.com>
2022-08-07 23:54:45 -0300

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,16 @@
+/*
+
+Desenvolvido por Raul Rodrigues
+Perfil do LinkedIn >>> https://www.linkedin.com/in/raul-de-souza-rodrigues-4135471b3/
+Perfil do Instagram >> https://www.instagram.com/rodriguesrauldesouza/
+
+*/
+#include <stdio.h>		// Inclui biblioteca de input/output
+
+int
+main ()				//funC'C#o principal do sistema
+{
+  printf ("Hello World");	//imprime na tela a mensagem passada entre aspas
+
+  return 0;
+}
